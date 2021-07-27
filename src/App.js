bf061c9a84028db61c9d6da41a8dbce7b0f6b365
Micky Balladelli <micky.balladelli@gmail.com>
2021-07-27 13:36:37 +0200

    utils

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import logo from './logo.svg'
 import './App.css'
+import { csvToArray } from './utils/utils.js'
 
 function App() {
-
-  function csvToArray(str, delimiter = ';') {
-    // slice from start of text to the first \n index
-    // use split to create an array from string by delimiter
-    const headers = str.slice(0, str.indexOf('\n')).split(delimiter)
-  
-    // slice from \n index + 1 to the end of the text
-    // use split to create an array of each csv value row
-    const rows = str.slice(str.indexOf('\n') + 1).split('\n')
-  
-    // Map the rows
-    // split values from each row into an array
-    // use headers.reduce to create an object
-    // object properties derived from headers:values
-    // the object passed as an element of the array
-    const arr = rows.map(function (row) {
-      const values = row.split(delimiter)
-      const el = headers.reduce(function (object, header, index) {
-        object[header] = values[index]
-        return object
-      }, {})
-      return el
-    })
-  
-    // return the array
-    return arr
-  }
-
 
   fetch('/data/19a91d64-3cd3-42fc-9943-d635491a4d76')
   .then(response => {
