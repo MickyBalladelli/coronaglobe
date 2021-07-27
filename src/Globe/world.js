2447fe01db872c8bf0f443ddfb7d5a28c3a77ec5
Micky Balladelli <micky.balladelli@gmail.com>
2021-07-27 18:53:58 +0200

    city labels

--- src/Globe/world.js
+++ src/Globe/world.js
@@ -5,15 +5,19 @@ import * as d3 from "d3"
 const World = () => {
   const globeEl = useRef()
   const [popData, setPopData] = useState([])
+  const [places, setPlaces] = useState([])
 
   useEffect(() => {
     // load data
     fetch('/datasets/world_population.csv').then(res => res.text())
     .then( csv => {
-      console.log(csv)
       return d3.csvParse(csv, ({ lat, lng, pop }) => ({ lat: +lat, lng: +lng, pop: +pop }))
     })
     .then(setPopData)
+
+    fetch('/datasets/ne_110m_populated_places_simple.geojson')
+    .then(res => res.json())
+    .then(({ features }) => setPlaces(features))
   }, [])
 
   useEffect(() => {
@@ -39,6 +43,16 @@ const World = () => {
     hexSideColor={d => weightColor(d.sumWeight)}
     hexBinMerge={true}
     enablePointerInteraction={false}
+
+    labelsData={places}
+    labelLat={d => d.properties.latitude}
+    labelLng={d => d.properties.longitude}
+    labelText={d => d.properties.name}
+    labelSize={d => Math.sqrt(d.properties.pop_max) * 4e-4}
+    labelDotRadius={d => Math.sqrt(d.properties.pop_max) * 4e-4}
+    labelColor={() => 'rgba(255, 165, 0, 0.75)'}
+    labelResolution={2}
+
   />
 }
 
