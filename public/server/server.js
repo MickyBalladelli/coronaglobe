99b4c791f84a5781e9ed319ac29f13bfc9388b6b
Micky Balladelli <micky.balladelli@gmail.com>
2021-07-27 10:45:34 +0200

    comments and fixes

--- public/server/server.js
+++ public/server/server.js
@@ -1,13 +1,11 @@
 // to use:
 // node build/server/server.js
 
-// include dependencies
 const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 const morgan = require("morgan")
 const path = require('path')
 
-// mount `exampleProxy` in web server
 const app = express()
 
 const SERVICE_URL = 'https://www.data.gouv.fr/fr/datasets/r/19a91d64-3cd3-42fc-9943-d635491a4d76'
@@ -21,5 +19,6 @@ app.use('/data', morgan('dev'), createProxyMiddleware({
   },
 }))
 
+// Static server relative to path build/server/../
 app.use('/', morgan('dev'), express.static(path.join(__dirname, '..')))
 app.listen(3000)
