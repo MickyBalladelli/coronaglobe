1742b5071192a02c7b1a1ee6bfd90df2fcad0e4d
Micky Balladelli <micky.balladelli@gmail.com>
2021-07-27 10:00:14 +0200

    morgan

--- public/server/server.js
+++ public/server/server.js
@@ -1,13 +1,14 @@
 // include dependencies
 const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware')
+const morgan = require("morgan")
 
 // mount `exampleProxy` in web server
 const app = express()
 
 
 // Proxy endpoints
-app.use('/data', createProxyMiddleware({
+app.use('/data', morgan('dev'), createProxyMiddleware({
     target: API_SERVICE_URL,
     changeOrigin: true,
     pathRewrite: {
