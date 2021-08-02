ab9f02fb7db792111f8d0167d3000a58891f6e82
Micky Balladelli <micky.balladelli@gmail.com>
2021-08-02 21:57:51 +0200

    cleanup

--- src/Appbar.js
+++ src/Appbar.js
@@ -3,10 +3,6 @@ import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import useCustom from './CustomHooks'
 import DisplayType from './DisplayType'
 import Filter from './Filter'
 
@@ -26,7 +22,6 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Appbar() {
   const classes = useStyles()
-  const [globalState, setGlobalState] = useCustom()
 
   return (
     <div className={classes.root}>
@@ -35,12 +30,8 @@ export default function Appbar() {
           <Typography variant="h6" className={classes.title}>
             Coronavirus Globe
           </Typography>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Filter />
           <DisplayType />
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
