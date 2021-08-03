68cf4a0e7aa5dd95a4b0cfcf9f69fbed0bb7473f
Micky Balladelli <micky.balladelli@gmail.com>
2021-08-03 11:27:13 +0200

    proxy is reserved ? use createProxy

--- public/server.js
+++ public/server.js
@@ -2,14 +2,14 @@
 // node build/server/server.js
 
 const express = require('express')
-const { proxy } = require('http-proxy-middleware')
+const { createProxy } = require('http-proxy-middleware')
 const morgan = require("morgan")
 
 const app = express()
 
 // from https://github.com/owid/covid-19-data
 
-app.use('/owid', morgan('dev'), proxy({
+app.use('/owid', morgan('dev'), createProxy({
   target: 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest',
   changeOrigin: true,
   pathRewrite: {
