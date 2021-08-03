fa0281f0a5817f8ada90ab7b594c55515d5f3fab
Micky Balladelli <micky.balladelli@gmail.com>
2021-08-03 11:27:46 +0200

    createProxyMiddleware

--- public/server.js
+++ public/server.js
@@ -2,14 +2,14 @@
 // node build/server/server.js
 
 const express = require('express')
-const { createProxy } = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 const morgan = require("morgan")
 
 const app = express()
 
 // from https://github.com/owid/covid-19-data
 
-app.use('/owid', morgan('dev'), createProxy({
+app.use('/owid', morgan('dev'), createProxyMiddleware({
   target: 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest',
   changeOrigin: true,
   pathRewrite: {
