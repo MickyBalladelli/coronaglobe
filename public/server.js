327243c4660adf1905cda1b00fb6577132f36d2b
Micky Balladelli <micky.balladelli@gmail.com>
2021-08-03 11:25:02 +0200

    simplify server

--- public/server.js
+++ public/server.js
@@ -2,15 +2,14 @@
 // node build/server/server.js
 
 const express = require('express')
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const { proxy } = require('http-proxy-middleware')
 const morgan = require("morgan")
-const path = require('path')
 
 const app = express()
 
 // from https://github.com/owid/covid-19-data
 
-app.use('/owid', morgan('dev'), createProxyMiddleware({
+app.use('/owid', morgan('dev'), proxy({
   target: 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest',
   changeOrigin: true,
   pathRewrite: {
