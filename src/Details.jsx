e152a6a6a93e8c0e294e635fbbcc9ddb53199cec
MickyBalladelli <micky.balladelli@gmail.com>
2026-05-15 22:53:22 +0200

    footer

--- src/Details.jsx
+++ src/Details.jsx
@@ -20,7 +20,7 @@ const FooterContainer = styled('div')({
   backgroundColor: 'rgba(0, 0, 0, 0.8)',
   color: '#fff',
   padding: '10px',
-  textAlign: 'center',
+  textAlign: 'flex-start',
   zIndex: 3000,
 })
 
@@ -50,75 +50,31 @@ export default function Details (props) {
      // Hantavirus-specific display
    if (props.disease === 'hanta') {
      return (
-           <RootContainer>
-             <div>    
-               <Typo style={{ "color": '#fff' }} variant="h4">{selected.country}</Typo><br/>
-               <Typo style={{ "color": '#fff' }} variant="caption">Last updated: {selected.date}</Typo><br/>
+           <div>
+             <RootContainer>
+               <div>    
+                 <Typo style={{ "color": '#fff' }} variant="h4">{selected.country}</Typo><br/>
+                 <Typo style={{ "color": '#fff' }} variant="caption">Last updated: {selected.date}</Typo><br/>
+                 <div>
+                   <Typo style={{ "color": '#fff' }} variant="caption">Confirmed: {selected.confirmed}</Typo><br/>
+                   </div>
+                 <div>
+                   <Typo style={{ "color": '#fff' }} variant="caption">Suspected: {selected.suspected}</Typo><br/>
+                   </div>
+                 <div>
+                   <Typo style={{ "color": '#fff' }} variant="caption">Deaths: {selected.deaths}</Typo><br/>
+                   </div>
+               </div> 
+             </RootContainer>
+             <FooterContainer>
                <div>
-                 <Typo style={{ "color": '#fff' }} variant="caption">Confirmed: {selected.confirmed}</Typo><br/>
-                 </div>
-               <div>
-                 <Typo style={{ "color": '#fff' }} variant="caption">Suspected: {selected.suspected}</Typo><br/>
-                 </div>
-               <div>
-                 <Typo style={{ "color": '#fff' }} variant="caption">Deaths: {selected.deaths}</Typo><br/>
-                 </div>
-             </div> 
-           </RootContainer>
+                 <Typo style={{ "color": '#fff' }} variant="caption">Status: {selected.status}</Typo><br/>
+                 <Typo style={{ "color": '#fff' }} variant="caption">Source: <a href={selected.source_url} target="_blank" rel="noopener noreferrer" style={{ color: '#4fc3f7' }}>{selected.source_url}</a></Typo><br/>
+               </div>
+             </FooterContainer>
+           </div>
           )
          }
-
-    // COVID-19 display (original)
-  return (
-        <RootContainer>
-          { globalState.selected !== null && globalState.selected !== undefined &&
-            <div>    
-              <Typo style={{ "color": '#fff' }} variant="h4">{globalState.selected.country}</Typo><br/>
-              <Typo style={{ "color": '#fff' }} variant="caption">Last updated: {globalState.selected.date}</Typo><br/>
-              {globalState.selected.new_cases &&
-              <div>
-                <Typo style={{ "color": getColor(globalState.selected.new_cases.color) }} variant="caption">New cases: {globalState.selected.new_cases.value}</Typo><br/>
-                </div>
-              }
-              {globalState.selected.new_deaths &&
-              <div>
-                <Typo style={{ "color": getColor(globalState.selected.new_deaths.color) }} variant="caption">New deaths: {globalState.selected.new_deaths.value}</Typo><br/>
-                </div>
-              }
-              {globalState.selected.icu_patients &&
-              <div>
-                <Typo style={{ "color": getColor(globalState.selected.icu_patients.color) }} variant="caption">ICU patients: {globalState.selected.icu_patients.value}</Typo><br/>
-                </div>
-              }
-              {globalState.selected.hosp_patients &&
-              <div>
-                <Typo style={{ "color": getColor(globalState.selected.hosp_patients.color) }} variant="caption">Hospitalized patients: {globalState.selected.hosp_patients.value}</Typo><br/>
-                </div>
-              }
-              {globalState.selected.total_deaths &&
-              <div>
-                <Typo style={{ "color": getColor(globalState.selected.total_deaths.color) }} variant="caption">Total deaths: {globalState.selected.total_deaths.value}</Typo><br/>
-                </div>
-              }
-              {globalState.selected.total_cases &&
-              <div>
-                <Typo style={{ "color": getColor(globalState.selected.total_cases.color) }} variant="caption">Total cases: {globalState.selected.total_cases.value}</Typo><br/>
-                </div>
-              }   
-              {globalState.selected.new_cases_per_million &&
-                <div>
-                  <Typo style={{ "color": getColor(globalState.selected.new_cases_per_million.color) }} variant="caption">New cases per million: {globalState.selected.new_cases_per_million.value}</Typo><br/>
-                  </div>
-              }   
-              {globalState.selected.new_deaths_per_million &&
-                  <div>
-                    <Typo style={{ "color": getColor(globalState.selected.new_deaths_per_million.color) }} variant="caption">New deaths per million: {globalState.selected.new_deaths_per_million.value}</Typo><br/>
-                    </div>
-              }   
-            </div> 
-          }
-        </RootContainer>
-       )
 
     // COVID-19 display (original)
   return (
